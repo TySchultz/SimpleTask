2d86249e9479ceabd12da7b324235c75d36617ca
TySchultz <Tyschultz7@gmail.com>
2015-12-02 16:37:29 -0500

    Initial Commit

---
+++ SimpleTaskTests/SimpleTaskTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleTaskTests.swift
+//  SimpleTaskTests
+//
+//  Created by Ty Schultz on 12/2/15.
+//  Copyright © 2015 Ty Schultz. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleTask
+
+class SimpleTaskTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
